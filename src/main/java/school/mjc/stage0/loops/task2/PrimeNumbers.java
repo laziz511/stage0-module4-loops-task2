e706eae0941b03604eb945279ef63d5bf5556cc8
Laziz Djuraev <110926594+laziz511@users.noreply.github.com>
2022-08-23 14:44:26 +0500

    Update PrimeNumbers.java

--- src/main/java/school/mjc/stage0/loops/task2/PrimeNumbers.java
+++ src/main/java/school/mjc/stage0/loops/task2/PrimeNumbers.java
@@ -2,5 +2,20 @@ package school.mjc.stage0.loops.task2;
 
 public class PrimeNumbers {
     public void printPrimeNumbers(int printToInclusive) {
+        if (printToInclusive >= 2)
+            System.out.println("2");
+        else return;
+        int sqr;
+
+        outer:
+        for(int i = 3; i <= printToInclusive; i+=2) {
+            sqr = (int) Math.sqrt(i) + 1;
+            for (int k = 3; k <= sqr; k += 2) {
+                if (i % k == 0)
+                    continue outer;
+            }
+            System.out.println(i);
+        }
+        
     }
 }
